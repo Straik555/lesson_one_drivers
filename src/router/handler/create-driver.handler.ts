5c31f8179378034bd268716a1a0dcc48d3f05c77
Vitalii Zakharchuk <95skin95@gmail.com>
2026-04-23 12:02:17 -0300

    create update, delete and testing.router.ts

--- src/router/handler/create-driver.handler.ts
+++ src/router/handler/create-driver.handler.ts
@@ -1,18 +1,18 @@
 import { Response } from "express";
 import { RequestWithBody } from "../../core/request-general.type";
-import { CreateDriverModelType } from "../model/create-driver.model";
+import { CreateUpdateDriverModel } from "../model/create-update-driver.model";
 import { ErrorsResponse } from "../../core/errors.type";
 import { HTTP_STATUS } from "../../core/http-status.type";
 import { mockDb } from "../../db/mock.db";
-import { createDriverValidation } from "../../validation/create-driver-validation";
+import { createUpdateDriverValidation } from "../../validation/create-update-driver-validation";
 
 export const createDriverHandler = (
-  req: RequestWithBody<CreateDriverModelType>,
+  req: RequestWithBody<CreateUpdateDriverModel>,
   res: Response<ErrorsResponse | DriverType>,
 ) => {
   const { body } = req;
   const errorsMessage: ErrorsResponse = { errorsMessages: [] };
-  createDriverValidation(body, errorsMessage);
+  createUpdateDriverValidation(body, errorsMessage);
 
   if (errorsMessage.errorsMessages.length > 0) {
     res.status(HTTP_STATUS.BAD_REQUEST_400).send(errorsMessage);
